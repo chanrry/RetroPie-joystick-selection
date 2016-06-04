20a860dfef447a371ff8becd596e0470823a50a0
meleu <meleu@users.noreply.github.com>
2016-06-04 14:19:31 -0300

    Create jslist.py

---
+++ jslist.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# jslist.py
+#
+# This little program just list the joysticks connected to the system.
+# The ouput format is "index:JoystickName".
+# It returns a non-zero value if no joystick was found; otherwise, zero.
+
+import pygame
+
+pygame.joystick.init()
+
+num_joy = pygame.joystick.get_count()
+
+if num_joy < 1:
+    print("No joystick found!")
+    pygame.joystick.quit()
+    quit(-1)
+
+for i in range(num_joy):
+    print(i,":",pygame.joystick.Joystick(i).get_name())
+
+pygame.joystick.quit()
